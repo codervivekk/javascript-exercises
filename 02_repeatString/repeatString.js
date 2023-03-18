35a01a9e31012a3821a1a50e969012adaeb39bd8
Vivek <codervivekk@gmail.com>
2023-03-18 12:51:25 +0530

    Assignment Complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(val,n) {
+    let str='';
+    if(n<0)
+        return 'ERROR';
+    for(let i=0;i<n;i++){
+        str+=val;
+    }
+    return str;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(strVal) {
+    let rev='';
+    let n = strVal.length;
+    for(let i=n-1;i>=0;i--){
+        rev+=strVal[i];
+    }
+    return rev;
 };
 
 // Do not edit below this line
